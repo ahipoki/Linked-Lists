6228cd97e8332478ccabcf88423b700594483cc1
ahipoki <50777654+ahipoki@users.noreply.github.com>
2019-12-04 14:02:07 -0800

    Update Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,3 +1,18 @@
 #include <iostream>
 #include <cstring>
 #include "Node.h"
+
+Node::Node(char* newNode)
+{
+  node = newNode;
+}
+
+Node* Node::getNext()
+{
+  return node;
+}
+
+void Node::setNext(Node*)
+{
+  
+}
